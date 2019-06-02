0a35bb8f4facc2c439a1dc41bb0f7eb2b2993e54
Emily <lalonde0902@gmail.com>
2019-06-02 13:38:01 -0600

    Set up class with its properties in the todo-list.js file

---
+++ todo-list.js
@@ -0,0 +1,19 @@
+class ToDoList {
+  constructor(list) {
+    this.id = list.id;
+    this.title = list.title;
+    this.urgent = list.urgent;
+    this.tasks = list.tasks;
+  }
+  saveToStorage() {
+  }
+
+  deleteFromStorage() {
+  }
+
+  updateToDo() {
+  }
+
+  updateTaskItems() {
+  }
+}
